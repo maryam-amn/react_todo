a200f33f6c69b38b17bb6628827496ad26a34486
maryam-amn <Maryam.Aman@jobtrek.ch>
2025-02-24 13:05:26 +0100

    chore: the page github is activited

---
+++ react_todo/rsbuild.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
+
+export default defineConfig({
+  plugins: [pluginReact()],
+  output: {
+    // Please replace <REPO_NAME> with the repository name.
+    // For example, "/my-project/"
+    assetPrefix: '/<react_todo>/',
+  }
+});
